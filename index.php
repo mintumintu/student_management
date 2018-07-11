d0bfcaeeb3e2b463b61cd2897278958082b9b6c4
mintumintu <41105213+mintumintu@users.noreply.github.com>
2018-07-11 15:23:29 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script type="text/javascript" href="js/home.js">
+<title>Welcome to Student Management System</title>
+</head>
+<body>
+<div class="bgimage">
+<div class="header">
+<h1>Welcome to Student Management System</h1>
+</div>
+<div class="login">
+<form action="php/action.php" method="post">
+<input type="email"><br>
+<input type="password"><br>
+<input type="submit"><input type="button" value="cancle">
+</div>
+</div>
+<hr>
+</body>
+</html>
